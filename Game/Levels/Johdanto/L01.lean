9ccda2e4c0025379ec5741ee75bb4bd002b0caac
Niklas Halonen <niklas.halonen@protonmail.com>
2025-10-05 08:26:54 +0300

    .

--- Game/Levels/Johdanto/L01.lean
+++ Game/Levels/Johdanto/L01.lean
@@ -10,13 +10,16 @@ Tässä kentässä todistamme seuraavan väitteen: Socrates on ihminen, kun tied
 
 Todistus etenee seuraavasti. Oletuksista tiedämme, että Socrates on ihminen, joten Socrates on ihminen. □
 
+□-merkkiä käytetään ilmaisemaan, että todistus on valmis.
+
 ## Oliot ja oletukset
 
-Lean esittää näytön alareunassa tärkeää tietoa tehtävän tilanteesta.
+Lean esittää näytön alareunassa tärkeää tietoa tehtävän tilasta.
+Tehtävän tila riippuu siitä millä rivillä editointikursori on.
 
 **Objects**-otsikon alla on listattu tehtävässä käytettyjen alkioiden tyypit. Alkio `Socrates_is_human` on tyyppiä `Prop`. `Prop` on loogisten väitteiden tyyppi, ja kaikki sen alkiot ovat loogisia väitteitä, eli `Socrates_is_human` on looginen väite. Myös esimerkiksi `1 = 2` on tyyppiä `Prop`.
 
-**Assumptions**-otsikon alla on tehtävänannossa esitetyt oletukset ja niiden tyypit. Oletus `h` on tyyppiä `Socrates_is_human`, eli se on todistus väitteestä Socrates on ihminen. Vinkki: sijoittamalla kursori `Socrates_is_human` tyypin päälle, saat lisätietoa tyypistä.
+**Assumptions**-otsikon alla on tehtävänannossa esitetyt oletukset ja niiden tyypit. Oletus `h` on tyyppiä `Socrates_is_human`, eli se on todistus väitteestä Socrates on ihminen.
 
 Koska tavoitteena on todistaa väite, mille `h` on jo todistus, voimme käyttää `h`:ta todistuksena käyttämällä `exact`-taktiikkaa.
 
